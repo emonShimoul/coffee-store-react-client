a10717f56b3f5b3cd2ee5444885a727251e18d21
emonShimoul <emonshimoul@gmail.com>
2025-02-25 02:46:34 +0600

    design add coffee form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,7 +1,99 @@
 const AddCoffee = () => {
+  const handleAddCoffe = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>Add a Coffee</h2>
+    <div className="bg-[#F4F3F0] p-24">
+      <h2 className="mb-4 text-3xl font-extrabold">Add a Coffee</h2>
+      <form onSubmit={handleAddCoffe} className="flex flex-col gap-4">
+        {/* form name and quantity row */}
+        <div className="md:flex gap-4">
+          <div className="md:w-1/2">
+            <input
+              type="text"
+              name="name"
+              placeholder="Coffee Name"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+
+          <div className="md:w-1/2">
+            <input
+              type="text"
+              name="quantity"
+              placeholder="Available Quantity"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+        </div>
+
+        {/* form supplier and taste row */}
+        <div className="md:flex gap-4">
+          <div className="md:w-1/2">
+            <input
+              type="text"
+              name="supplier"
+              placeholder="Supplier Name"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+
+          <div className="md:w-1/2">
+            <input
+              type="text"
+              name="taste"
+              placeholder="Taste"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+        </div>
+
+        {/* form category and details row */}
+        <div className="md:flex gap-4">
+          <div className="md:w-1/2">
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+
+          <div className="md:w-1/2">
+            <input
+              type="text"
+              name="details"
+              placeholder="Details"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+        </div>
+
+        {/* form row */}
+        <div className="">
+          <div className="w-full">
+            <input
+              type="text"
+              name="photo"
+              placeholder="Photo URL"
+              className="w-full bg-white p-3 rounded border border-gray-300"
+              required
+            />
+          </div>
+        </div>
+
+        <input
+          type="submit"
+          value="Add Coffee"
+          className="btn btn-block bg-amber-700 text-white font-bold"
+        />
+      </form>
     </div>
   );
 };
