9633e5b8a933aa4d8ff8281f8034be1b87b6c858
emonShimoul <emonshimoul@gmail.com>
2025-02-25 01:37:06 +0600

    create some route and component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-red-500 font-bold">Coffee Store</h1>
-      <span className="loading loading-spinner text-primary"></span>
-      <span className="loading loading-spinner text-secondary"></span>
-      <span className="loading loading-spinner text-accent"></span>
-      <span className="loading loading-spinner text-neutral"></span>
-      <span className="loading loading-spinner text-info"></span>
-      <span className="loading loading-spinner text-success"></span>
-      <span className="loading loading-spinner text-warning"></span>
-      <span className="loading loading-spinner text-error"></span>
+      <h1 className="text-6xl text-purple-700">Coffee Store</h1>
     </>
   );
 }
