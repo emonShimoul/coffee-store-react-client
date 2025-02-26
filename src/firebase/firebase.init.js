0bfc133b5fac3aec004dd601f22de0254e95e7b3
emonShimoul <emonshimoul@gmail.com>
2025-02-26 12:45:32 +0600

    create sign up form

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLI9ePGXspJWMwm6hViRFZUMaqLPArQC4",
+  authDomain: "coffee-store-ebeba.firebaseapp.com",
+  projectId: "coffee-store-ebeba",
+  storageBucket: "coffee-store-ebeba.firebasestorage.app",
+  messagingSenderId: "228148910780",
+  appId: "1:228148910780:web:d549784106e9d42bfaceb9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
