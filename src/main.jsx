ab09a4a474239f2b9d5918cba3338ec1d95e2f2a
emonShimoul <emonshimoul@gmail.com>
2025-02-26 12:52:54 +0600

    implement firebase and context api

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp.jsx";
 import Layout from "./components/Layout.jsx";
 import Home from "./components/Home.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -45,6 +46,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
