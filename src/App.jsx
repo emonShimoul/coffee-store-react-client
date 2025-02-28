76b3f7739910f8aa0058c5f07b96bce2faddb3ff
emonShimoul <emonshimoul@gmail.com>
2025-02-28 22:57:59 +0600

    remove unnecessary codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { useLoaderData } from "react-router-dom";
 import "./App.css";
-import CoffeeCard from "./components/CoffeeCard";
-import { useState } from "react";
 
 function App() {
-  const loadedCoffees = useLoaderData();
-
-  const [coffees, setCoffees] = useState(loadedCoffees);
-
   return (
     <div className="m-10">
       <h1 className="text-3xl font-bold text-center text-purple-700 mb-8">
-        Coffee Store: {coffees.length}
+        Coffee Store!!
       </h1>
-      <div className="grid md:grid-cols-2 gap-4">
-        {coffees.map((coffee) => (
-          <CoffeeCard
-            key={coffee._id}
-            coffee={coffee}
-            coffees={coffees}
-            setCoffees={setCoffees}
-          ></CoffeeCard>
-        ))}
-      </div>
     </div>
   );
 }
